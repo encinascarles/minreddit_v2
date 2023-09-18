1fb4fbf937baba72831fd7891ce223cf29544787
Carles Encinas <thecrax@ganxo.net>
2023-09-18 23:19:17 +0200

    Organized page content

--- src/pages/SubReddit.jsx
+++ src/pages/SubReddit.jsx
@@ -1,8 +1,25 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import SideNavigation from '../components/SideNavigation'
+import Posts from '../components/Posts';
+
+function SubRedditTitle() {
+  return (
+    <Box>
+        <h1>SubReddit Title</h1>
+    </Box>
+  );
+}
 
 const SubReddit = () => {
   return (
-    <div>SubReddit</div>
+    <Box display="flex" justifyContent="center" gap={4}>
+        <SideNavigation />
+        <Box display="flex" flexDirection="column">
+            <SubRedditTitle />
+            <Posts />
+        </Box>
+    </Box>
   )
 }
 
