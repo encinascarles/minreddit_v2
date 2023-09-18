ebe2a131877984a96fa09c1dbabfa27c2644c7b1
Carles Encinas <thecrax@ganxo.net>
2023-09-18 23:33:58 +0200

    Side Navigation implemented

--- src/components/SideNavigation.jsx
+++ src/components/SideNavigation.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import {
+  Box,
+  Collapse,
+  Container,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import VideogameAssetOutlinedIcon from "@mui/icons-material/VideogameAssetOutlined";
+import SportsBaseballOutlinedIcon from "@mui/icons-material/SportsBaseballOutlined";
+import ShowChartOutlinedIcon from "@mui/icons-material/ShowChartOutlined";
+import CurrencyBitcoinOutlinedIcon from "@mui/icons-material/CurrencyBitcoinOutlined";
+import TvOutlinedIcon from "@mui/icons-material/TvOutlined";
+import StarsOutlinedIcon from "@mui/icons-material/StarsOutlined";
+
+import React from "react";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+
+const temas = {
+  Videojuegos: {
+    icon: <VideogameAssetOutlinedIcon />,
+    subtemas: [
+      "Valheim",
+      "Genshin Impact",
+      "Minecraft",
+      "Pokimane",
+      "Halo Infinite",
+    ],
+  },
+  Deportes: {
+    icon: <SportsBaseballOutlinedIcon />,
+    subtemas: [
+      "Valheim",
+      "Genshin Impact",
+      "Minecraft",
+      "Pokimane",
+      "Halo Infinite",
+    ],
+  },
+  Negocios: {
+    icon: <ShowChartOutlinedIcon />,
+    subtemas: [
+      "Valheim",
+      "Genshin Impact",
+      "Minecraft",
+      "Pokimane",
+      "Halo Infinite",
+    ],
+  },
+  Crypto: {
+    icon: <CurrencyBitcoinOutlinedIcon />,
+    subtemas: [
+      "Valheim",
+      "Genshin Impact",
+      "Minecraft",
+      "Pokimane",
+      "Halo Infinite",
+    ],
+  },
+  Television: {
+    icon: <TvOutlinedIcon />,
+    subtemas: [
+      "Valheim",
+      "Genshin Impact",
+      "Minecraft",
+      "Pokimane",
+      "Halo Infinite",
+    ],
+  },
+  Famosos: {
+    icon: <StarsOutlinedIcon />,
+    subtemas: [
+      "Valheim",
+      "Genshin Impact",
+      "Minecraft",
+      "Pokimane",
+      "Halo Infinite",
+    ],
+  },
+};
 
 const SideNavigation = () => {
-  return (
-    <div>SideNavigation</div>
-  )
-}
+  const [open, setOpen] = React.useState("");
 
-export default SideNavigation
+  const handleClick = (tema) => {
+    if (open === tema) {
+      setOpen(false);
+    } else {
+      setOpen(tema);
+    }
+  };
+
+  return (
+    <Box width="300px" >
+			<Box position="fixed" width="300px">
+      <Container>
+        <List>
+          {Object.keys(temas).map((text) => (
+            <>
+              <ListItemButton onClick={() => handleClick(text)}>
+                <ListItemIcon>{temas[text].icon}</ListItemIcon>
+                <ListItemText primary={text} />
+                {open === text ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open === text} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  {temas[text].subtemas.map((text) => (
+                    <ListItemButton sx={{ pl: 4 }}>
+                      <ListItemText primary={text} />
+                    </ListItemButton>
+                  ))}
+                </List>
+              </Collapse>
+            </>
+          ))}
+        </List>
+      </Container>
+			</Box>
+    </Box>
+  );
+};
+
+export default SideNavigation;
