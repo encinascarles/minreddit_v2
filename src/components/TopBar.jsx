4aa6225ef63320d0861cdc554732f8f323e05006
Carles <42646145+encinascarles@users.noreply.github.com>
2023-09-23 22:08:37 +0200

    Added nav to minReddit logo

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -4,6 +4,7 @@ import Brightness7Icon from '@mui/icons-material/Brightness7';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import { selectTheme, toggleMode } from '../theme/themeSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 
 const TopBar = () => {
@@ -12,7 +13,9 @@ const TopBar = () => {
   return (
     <AppBar position="sticky" sx={{backgroundColor:"primary.main"}}>
             <Toolbar sx={{display:"flex", justifyContent:"space-between"}}>
-                <Typography variant="h4"> minReddit </Typography>
+                <Link to="/">
+                  <Typography variant="h4"> minReddit </Typography>
+                </Link>
                 <IconButton sx={{color:"primary.contrastText"}} onClick={()=>dispatch(toggleMode())}>
                 {useSelector(selectTheme).palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
                 </IconButton>
