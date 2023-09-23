77f706d2c18e25d102f9d3cfe331bc53f628a37c
Carles <42646145+encinascarles@users.noreply.github.com>
2023-09-23 20:48:15 +0200

    Linked Home components

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,17 +1,25 @@
-import { Avatar, Card, CardHeader } from "@mui/material";
+import { Avatar, Card, CardActionArea, CardHeader } from "@mui/material";
 import { red } from "@mui/material/colors";
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 
-const HomeCard = ({SubRedditInfo}) => {
+
+const HomeCard = ({ SubRedditInfo }) => {
   return (
-    <Card sx={{width:"235px",backgroundImage:"none"}}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe" src={SubRedditInfo.icon}/>
-        }
-        title={SubRedditInfo.name}
-        subheader={`${SubRedditInfo.members} members`}
-      />
+    <Card sx={{ width: "235px", backgroundImage: "none" }}>
+      <CardActionArea component={RouterLink} to={SubRedditInfo.link}>
+        <CardHeader
+          avatar={
+            <Avatar
+              sx={{ bgcolor: red[500] }}
+              aria-label="recipe"
+              src={SubRedditInfo.icon}
+            />
+          }
+          title={SubRedditInfo.name}
+          subheader={`${SubRedditInfo.members} members`}
+        />
+      </CardActionArea>
     </Card>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,73 +8,73 @@ const popularSubreddits = [
     name: "AskReddit",
     members: "43.345.234",
     icon: "https://styles.redditmedia.com/t5_2qh1i/styles/communityIcon_p6kb2m6b185b1.png",
-    link: "AskReddit"
+    link: "/r/AskReddit"
   },
   {
     name: "NoStupidQuestions",
     members: "3.914.346",
     icon: "https://styles.redditmedia.com/t5_2w844/styles/communityIcon_krq4riav5m191.png",
-    link: "NoStupidQuestions"
+    link: "/r/NoStupidQuestions"
   },
   {
     name: "Minecraft",
     members: "7.393.836",
     icon: "https://b.thumbs.redditmedia.com/rwN0al9P6nYhGSQO-yIJb-FyF5xg-c2v61zjMom4c4E.png",
-    link: "Minecraft"
+    link: "/r/Minecraft"
   },
   {
     name: "DnD",
     members: "3.362.736",
     icon: "https://styles.redditmedia.com/t5_2r9ei/styles/communityIcon_zv45roy38jlb1.png",
-    link: "Dnd"
+    link: "/r/Dnd"
   },
   {
     name: "videos",
     members: "26.831.394",
     icon: "https://styles.redditmedia.com/t5_2qh1e/styles/communityIcon_ostbin5fscdb1.png",
-    link: "videos"
+    link: "/r/videos"
   },
   {
     name: "Amd",
     members: "1.613.090",
     icon: "https://styles.redditmedia.com/t5_2rw0n/styles/communityIcon_098dz881tgk81.png",
-    link: "Amd"
+    link: "/r/Amd"
   },
   {
     name: "DunderMifflin",
     members: "2.264.836",
     icon: "https://styles.redditmedia.com/t5_2s9h3/styles/communityIcon_ujdrcdijabb61.png",
-    link: "DunderMifflin"
+    link: "/r/DunderMifflin"
   },
   {
     name: "Android",
     members: "2.562.645",
     icon: "https://b.thumbs.redditmedia.com/fI7UdJ-vgpnLdxy28QdKIYBGg-fEo7KxQ_PS7pn4QzM.png",
-    link: "Android"
+    link: "/r/Android"
   },
   {
     name: "Steam",
     members: "2.075.113",
     icon: "https://b.thumbs.redditmedia.com/xvwxkNXOkvdu9d6S67odp1gCPfhB1A3qKDs7kdwO5ts.png",
-    link: "Steam"
+    link: "/r/Steam"
   },
   {
     name: "memes",
     members: "28.749.927",
     icon: "https://styles.redditmedia.com/t5_2qjpg/styles/communityIcon_x0untei1wh9b1.png",
-    link: "memes"
+    link: "/r/memes"
   },
   {
     name: "wow",
     members: "2.395.106",
     icon: "https://styles.redditmedia.com/t5_2qio8/styles/communityIcon_f3a3cxfc6dra1.png",
-    link: "wow"
+    link: "/r/wow"
   },
   {
     name: "pokemongo",
     members: "4.493.196",
     icon: "https://b.thumbs.redditmedia.com/G8vrniZg3VHg6g-cDgBOnCdlsx2TEbiSX_CRA_cRV5I.png",
-    link: "pokemongo"
+    link: "/r/pokemongo"
   },
 
 ]
