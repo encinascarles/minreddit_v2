8e632b011ef167d4c6614f702db56c2f9334b717
Carles <42646145+encinascarles@users.noreply.github.com>
2023-09-23 16:25:47 +0200

    Home grid styling with blue box

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,33 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import React from "react";
 import HomeCard from "../components/HomeCard";
 
+function BlueBox() {
+  return (
+    <Grid item xs={6} sm={4} >
+      <Box
+        sx={{
+          width: "235px",
+          height: "76px",
+          backgroundColor: "blue",
+        }}
+      >aa</Box>
+    </Grid>
+
+  );
+}
 
 const Home = () => {
   return (
-    <Box>
-      <Grid container spacing={2}>
-        <Grid item xs={6} sm={4} >
-        <HomeCard/>
-        </Grid>
-        <Grid item xs={6} sm={4} >
-        <p>1</p>
-        </Grid>
-        <Grid item xs={6} sm={4} >
-        <p>1</p>
-        </Grid>
-        <Grid item xs={6} sm={4} >
-        <p>1</p>
-        </Grid>
-        <Grid item xs={6} sm={4} >
-        <p>1</p>
-        </Grid>
-        <Grid item xs={6} sm={4} >
-        <p>1</p>
-        </Grid>
-        <Grid item xs={6} sm={4} >
-        <p>1</p>
-        </Grid>
-        <Grid item xs={6} sm={4} >
-        <p>1</p>
-        </Grid>
-        <Grid item xs={6} sm={4} >
-        <p>1</p>
-        </Grid>
+    <Box display="flex" flexDirection="column" alignItems="center" paddingTop="38px" gap="38px">
+      <Typography variant="h2">Popular SubReddits</Typography>
+      <Grid container rowSpacing="25px" columnSpacing="46px" sx={{width:"calc(235px*3 + 25px*2)"}}>
+        {Array.from(Array(12).keys()).map((i) => (
+          <BlueBox />
+        ))
+      }
+        
       </Grid>
     </Box>
   );
